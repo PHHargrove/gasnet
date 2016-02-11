79b0d8a3d2e4c8208e79a046ab1e4a445fc29be9
Paul H. Hargrove <PHHargrove@lbl.gov>
2016-02-11 14:48:28 -0800

    ibv+blcr: generalized gasnetc_send_am_nc()
    This commit replaces gasnetc_send_am_close() with a more general
    function gasnetc_send_am_nc().  This enables sending Short AMs other
    than the "close", through addition of arguments for is_req, handler
    and args.  Additionally, the destination is now specified as a
    gasnetc_cep_t*, rather than gasnet_node_t, to allow the caller to
    send over a specific channel.
    
    This commit does not yet change the quiecse logic in any way.
    However, a later commit will use the added generality of this
    function to implement flushing of coallesced credits.

--- ibv-conduit/gasnet_core_sndrcv.c
+++ ibv-conduit/gasnet_core_sndrcv.c
@@ -1993,6 +1993,7 @@ int gasnetc_ReqRepGeneric(gasnetc_category_t category, gasnetc_rbuf_t *token,
 #endif
       epid = gasnetc_epid(dest, qpi + qp_offset);
       cep += qpi;
+      gasneti_assert(epid == cep->epid);
     }
   
     /* Reserve space for extra arguments if we *might* carry flow control
@@ -3576,28 +3577,30 @@ void gasnetc_sys_close_reqh(gasnet_token_t token) {
   rbuf->rbuf_needReply = 0; /* we are terminating flow control */
 }
 
-/* Sends a AM Short with no args WITHOUT obtaining an AM credit.
- * The remote handler is responsible for suppressing the return of the credit.
- */
+/* Sends an AM Short with WITHOUT obtaining an AM credit. */
+/* BLCR-TODO: can/should we factor this subset out of ReqRepGeneric? */
 static void
-gasnetc_send_am_close(gasnet_node_t dest) {
-  const int handler = gasneti_handleridx(gasnetc_sys_close_reqh);
+gasnetc_send_am_nc(gasnetc_cep_t *cep, int is_req, int handler, int numargs, ...) {
   GASNETC_DECL_SR_DESC(sr_desc, 1);
   gasnetc_sreq_t *sreq;
+  va_list argptr;
+  int i;
 
   /* Buffer management */
   const size_t min_len = GASNETC_ALLOW_0BYTE_MSG ? 0 : 4;
-  const size_t msg_len = MAX(GASNETC_MSG_SHORT_ARGSEND(0), min_len);
+  const size_t msg_len = MAX(GASNETC_MSG_SHORT_ARGSEND(numargs), min_len);
   const int is_inline = (msg_len <= gasnetc_inline_limit);
   gasnetc_shortmsg_t tmp_buf;
   gasnetc_buffer_t *buf = is_inline ? (gasnetc_buffer_t*)(&tmp_buf) : gasnetc_get_bbuf(1);
 
-  /* Get cep and epid for the first AM Request channel */
-  const int qp_offset = gasnetc_use_srq ? gasnetc_num_qps : 0;
-  gasnetc_cep_t *cep = gasnetc_get_cep(dest) + qp_offset;
-  gasnetc_epid_t epid = gasnetc_epid(dest, qp_offset);
+  /* copy args */
+  va_start(argptr, numargs);
+  for (i=0; i < numargs; ++i) {
+    buf->shortmsg.args[i] = va_arg(argptr, gasnet_handlerarg_t);
+  }
+  va_end(argptr);
 
-  sr_desc->imm_data   = GASNETC_MSG_GENFLAGS(1, gasnetc_Short, 0, handler, gasneti_mynode);
+  sr_desc->imm_data   = GASNETC_MSG_GENFLAGS(is_req, gasnetc_Short, numargs, handler, gasneti_mynode);
   sr_desc->opcode     = IBV_WR_SEND_WITH_IMM;
   sr_desc->num_sge    = 1;
   sr_desc->sg_list[0].addr   = (uintptr_t)buf;
@@ -3613,7 +3616,7 @@ gasnetc_send_am_close(gasnet_node_t dest) {
     buf->stamp = GASNETI_TICKS_NOW_IFENABLED(C);
   #endif
 
-  (void)gasnetc_bind_cep_inner(epid, sreq, IBV_WR_SEND_WITH_IMM, msg_len, 0);
+  (void)gasnetc_bind_cep_inner(cep->epid, sreq, IBV_WR_SEND_WITH_IMM, msg_len, 0);
   gasnetc_snd_post_common(sreq, sr_desc, is_inline);
 }
 
@@ -3653,7 +3656,9 @@ gasnetc_sndrcv_quiesce(void) {
     for (shift = 0, distance = 1; distance < gasneti_nodes; ++shift, distance *= 2) {
       gasnet_node_t peer = (distance <= gasneti_mynode) ? gasneti_mynode - distance
                                                         : gasneti_mynode + (gasneti_nodes - distance);
-      gasnetc_send_am_close(peer);
+      const int qp_offset = gasnetc_use_srq ? gasnetc_num_qps : 0;
+      gasnetc_cep_t *cep = gasnetc_get_cep(peer) + qp_offset;
+      gasnetc_send_am_nc(cep, 1, gasneti_handleridx(gasnetc_sys_close_reqh), 0);
       while (! gasnetc_close_recvd[shift]) {
         GASNETI_WAITHOOK();
         gasnetc_poll_both();
