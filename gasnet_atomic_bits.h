93a834c6a16df5d13e430174d86698ac4bb27217
Paul H. Hargrove <PHHargrove@lbl.gov>
2018-04-19 10:10:35 -0700

    atomic: uniform native atomic32_t
    This commit changes the typedef for atomic32_t for ARM to match all
    other platforms

--- gasnet_atomic_bits.h
+++ gasnet_atomic_bits.h
@@ -2520,7 +2520,7 @@
     #if PLATFORM_OS_LINUX && (PLATFORM_COMPILER_GNU || PLATFORM_COMPILER_CLANG)
       #define GASNETI_HAVE_ATOMIC32_T 1
 
-      typedef struct { volatile unsigned int ctr; } gasneti_atomic32_t;
+      typedef struct { volatile uint32_t ctr; } gasneti_atomic32_t;
       #define gasneti_atomic32_init(v)       { (v) }
       #define _gasneti_atomic32_read(p)      ((p)->ctr)
       #define _gasneti_atomic32_set(p,v)     ((p)->ctr = (v))
