f5da89476966ebc4c36972b6efcfc47428a9cff8
Paul H. Hargrove <PHHargrove@lbl.gov>
2018-11-14 14:38:18 -0600

    wip - update protection for internal headers

--- ofi-conduit/gasnet_core.h
+++ ofi-conduit/gasnet_core.h
@@ -5,8 +5,8 @@
  * Terms of use are as specified in license.txt
  */
 
-#ifndef _IN_GASNET_H
-  #error This file is not meant to be included directly- clients should include gasnet.h
+#ifndef _IN_GASNETEX_H
+  #error This file is not meant to be included directly- clients should include gasnetex.h
 #endif
 
 #ifndef _GASNET_CORE_H
