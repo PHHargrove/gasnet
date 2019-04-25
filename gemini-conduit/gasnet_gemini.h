99ae1c9ddfecf2ec22478725022d6f4ed0f819d4
Paul H. Hargrove <PHHargrove@lbl.gov>
2019-04-24 17:47:09 -0700

    aries/gemini: narrow initiator_slot to 10 bits
    This commit narrows the width of the "initiator_slot" used in AM
    communication from 16 bits to 10.  This means that the maximum number
    of AM Requests in-flight from a given process has been lowered from
    65536 to 1024.  Note, however, that the default of 64 is unchanged.
    
    Additionally, the constants GASNETC_AM_{INITIATOR,TARGET}_SLOTS are
    introduced to make any future changes to these field widths simpler
    (and less error prone).

--- gemini-conduit/gasnet_gemini.h
+++ gemini-conduit/gasnet_gemini.h
@@ -116,9 +116,12 @@ enum gc_notify_type {
 #define gc_build_notify(_type, _initiator, _target)\
   ((uint64_t)(_type) |  ((uint64_t)(_initiator) << 8) |  ((uint64_t)(_target)))
 
+#define GASNETC_AM_INITIATOR_SLOTS 1024 // 10 bits
+#define GASNETC_AM_TARGET_SLOTS      64 //  6 bits
+
 #define gc_notify_get_type(n) ((n) & 0xff000000)
-#define gc_notify_get_target_slot(n) ((uint8_t)((n) & 255)) /* actual range 0..63 */
-#define gc_notify_get_initiator_slot(n) ((uint16_t)(((n) >> 8) & 65535))
+#define gc_notify_get_target_slot(n) ((uint8_t)((n) & (GASNETC_AM_TARGET_SLOTS-1)))
+#define gc_notify_get_initiator_slot(n) ((uint16_t)(((n) >> 8) & (GASNETC_AM_INITIATOR_SLOTS-1)))
 
 // message type encoded in CQ data
 enum gc_cqdata_type {
