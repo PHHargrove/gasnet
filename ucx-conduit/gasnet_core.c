25e5e6b1542ac82845938f2a40d1bfeca26e3dba
Boris Karasev <karasev.b@gmail.com>
2019-07-26 10:44:11 +0600

    ucx-conduit: repair assertion seen in testinternal

--- ucx-conduit/gasnet_core.c
+++ ucx-conduit/gasnet_core.c
@@ -516,6 +516,15 @@ extern void gasnetc_exit(int exitcode) {
   gasneti_fatalerror("gasnetc_exit failed!");
 }
 
+// TODO-EX:
+// This conduit curently uses the extended-ref for all RMA.  That, in turn,
+// uses AMs in an NBI access region.  Unfortunately, that means one cannot run
+// progress functions that may communicate using NBI (or accurately then cannot
+// call gex_NB_Test()).  Otherwise, assertions regarding access regions result.
+//
+// This avoids the problem by omitting progress functions on entry to AMRequest.
+#define GASNETC_IMMEDIATE_MAYBE_POLL_NOPF(flag) \
+        do { if (GASNETC_IMMEDIATE_WOULD_POLL(flag)) gasnetc_AMPoll(); } while (0)
 /* ------------------------------------------------------------------------------------ */
 /*
   Misc. Active Message Functions
@@ -602,7 +611,7 @@ extern int gasnetc_AMRequestShortM(
                             GASNETI_THREAD_FARG,
                             int numargs, ...) {
   GASNETI_COMMON_AMREQUESTSHORT(tm,rank,handler,flags,numargs);
-  GASNETC_IMMEDIATE_MAYBE_POLL(flags); /* (###) poll at least once, to assure forward progress */
+  GASNETC_IMMEDIATE_MAYBE_POLL_NOPF(flags); /* (###) poll at least once, to assure forward progress */
 
   va_list argptr;
   va_start(argptr, numargs); /*  pass in last argument */
@@ -661,7 +670,7 @@ extern int gasnetc_AMRequestMediumM(
                             GASNETI_THREAD_FARG,
                             int numargs, ...) {
   GASNETI_COMMON_AMREQUESTMEDIUM(tm,rank,handler,source_addr,nbytes,lc_opt,flags,numargs);
-  GASNETC_IMMEDIATE_MAYBE_POLL(flags); /* (###) poll at least once, to assure forward progress */
+  GASNETC_IMMEDIATE_MAYBE_POLL_NOPF(flags); /* (###) poll at least once, to assure forward progress */
 
   va_list argptr;
   va_start(argptr, numargs); /*  pass in last argument */
@@ -876,7 +885,7 @@ extern int gasnetc_AMRequestLongM(
                             GASNETI_THREAD_FARG,
                             int numargs, ...) {
   GASNETI_COMMON_AMREQUESTLONG(tm,rank,handler,source_addr,nbytes,dest_addr,lc_opt,flags,numargs);
-  GASNETC_IMMEDIATE_MAYBE_POLL(flags); /* (###) poll at least once, to assure forward progress */
+  GASNETC_IMMEDIATE_MAYBE_POLL_NOPF(flags); /* (###) poll at least once, to assure forward progress */
 
   va_list argptr;
   va_start(argptr, numargs); /*  pass in last argument */
