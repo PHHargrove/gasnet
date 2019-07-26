3350c4d6a9246734a40765fa3f0aff7db9444298
Boris Karasev <karasev.b@gmail.com>
2019-07-26 12:17:06 +0600

    ucx-conduit: disable PSHMEM
    Since PSHM is not yet supported by ucx-conduit.

--- ucx-conduit/gasnet_core_fwd.h
+++ ucx-conduit/gasnet_core_fwd.h
@@ -21,8 +21,8 @@
 #define GASNET_CONDUIT_UCX 1
 
   /* GASNET_PSHM defined 1 if this conduit supports PSHM. leave undefined otherwise. */
-#if GASNETI_PSHM_ENABLED
-#define GASNET_PSHM 1 //###
+#if GASNETI_PSHM_ENABLED && 0 // this conduit not yet supported PSHM
+#define GASNET_PSHM 1
 #endif
 
   /*  defined to be 1 if gasnet_init guarantees that the remote-access memory segment will be aligned  */
