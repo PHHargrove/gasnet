503f712b4278db6ec6e5ecda757f185eeaefce01
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-03-01 21:30:25 -0800

    ibv: Initial native implementation of NPAM Long
    This commit provides an initial (complete/correct) native ibv-conduit
    implementation of `gex_AM_{Prepare,Commit}{Request,Reply}Long()` and
    corresponding adjustments to the max-payload queries.
    
    The majority of the changes are generalization of the internal
    functions `gasnetc_{prepare,commit}_medium()` to become
    `gasnetc_{prepare,commit}_common()` with the addition of a `category`
    argument.  This keeps a single version of the most complex code, and
    makes the reasonable assumption that the optimizer will specialize the
    inlined code using the manifest constant `category in the callers.
    
    The new callers of the common code are the "near public"
    `gasnetc_AM_...()` entry points called in `gasnet_ammacros.h` after
    insertion of `GASNETI_THREAD_GET` and handling of argument counts in
    the `Commit` calls.  These are simple clones of the Medium versions
    with the necessary mechanical changes for Medium-vs-Long.
    
    Benchmarking of the client-allocated buffer case shows a small
    degradation relative to the equivalent FPAM call.  This is expected
    since there is no benefit to be gained for the transfer itself, and
    the Prepare/Commit separation lengthens the critical path.
    
    For the GASNet-allocated case, this implementation unconditionally
    uses the "packed long" representation both in memory and for actual
    communication, even for payload sizes where packed long is known to
    under-perform in FPAM.  Future work will enable RDMA for appropriate
    payload sizes.

--- ibv-conduit/gasnet_core_fwd.h
+++ ibv-conduit/gasnet_core_fwd.h
@@ -23,6 +23,12 @@
 #define GASNET_CONDUIT_NAME_STR  _STRINGIFY(GASNET_CONDUIT_NAME)
 #define GASNET_CONDUIT_IBV       1
 
+#if defined(GASNET_SEGMENT_FAST)
+  #define GASNETC_PIN_SEGMENT 1
+#else
+  #define GASNETC_PIN_SEGMENT 0
+#endif
+
 // Size of a buffer to contain any AM with all its header, padding and payload
 #define GASNETC_BUFSZ GASNETC_IBV_MAX_MEDIUM
 
@@ -83,8 +89,10 @@
    */
 #define GASNETC_HAVE_NP_REQ_MEDIUM 1
 #define GASNETC_HAVE_NP_REP_MEDIUM 1
-/* #define GASNETC_HAVE_NP_REQ_LONG 1 */
-/* #define GASNETC_HAVE_NP_REP_LONG 1 */
+#if GASNETC_PIN_SEGMENT
+#define GASNETC_HAVE_NP_REQ_LONG 1
+#define GASNETC_HAVE_NP_REP_LONG 1
+#endif
 
   /* uncomment for each GASNETC_HAVE_NP_* enabled above if the Commit function
      has the numargs argument even in an NDEBUG build (it is always passed in
@@ -92,11 +100,14 @@
    */
 #define GASNETC_AM_COMMIT_REQ_MEDIUM_NARGS 1
 #define GASNETC_AM_COMMIT_REP_MEDIUM_NARGS 1
-//#define GASNETC_AM_COMMIT_REQ_LONG_NARGS 1
-//#define GASNETC_AM_COMMIT_REP_LONG_NARGS 1
+#if GASNETC_PIN_SEGMENT
+#define GASNETC_AM_COMMIT_REQ_LONG_NARGS 1
+#define GASNETC_AM_COMMIT_REP_LONG_NARGS 1
+#endif
 
 #define GASNETI_AM_SRCDESC_EXTRA \
         int                 _have_flow;         \
+        int                 _head_len;          \
         void *              _buf_alloc;         \
         void *              _cep;               \
         void *              _ep;                \
@@ -109,12 +120,6 @@
      Used if (and only if) any of the GASNETC_HAVE_NP_* values above are unset.
    */
 /* #define GASNETC_REQUESTV_POLLS 1 */
-
-#if defined(GASNET_SEGMENT_FAST)
-  #define GASNETC_PIN_SEGMENT 1
-#else
-  #define GASNETC_PIN_SEGMENT 0
-#endif
 
   // uncomment if conduit provides a gasnetc-prefixed override
   // TODO: this should be a hook rather than an override
