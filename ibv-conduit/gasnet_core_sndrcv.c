386216606c4d54aa5d720b15a9f620e3f80b748d
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-04-06 21:21:26 -0700

    ibv: correct protocol logic for bulk Put/Long
    This commit corrects protocol selection in the RMA Put and AM Long
    payload transfer paths to apply `GASNET_NONBULKPUT_BOUNCE_LIMIT`
    only to the "non-bulk" case of `lc_opt == GEX_EVENT_NOW`, where
    previously `lc_opt != GEX_EVENT_DEFER` was incorrectly used.
    
    Resolves bug 4209

--- ibv-conduit/gasnet_core_sndrcv.c
+++ ibv-conduit/gasnet_core_sndrcv.c
@@ -2168,8 +2168,9 @@ size_t gasnetc_fh_put_helper(
    * done by the put-in-move optimization, under the assumption that
    * the original request len is representative of future requests.
    */
+  int is_nonbulk = (sreq->fh_lc_cb == gasnetc_cb_counter); // GEX_EVENT_NOW
   if ((len <= gasnetc_inline_limit) ||
-	((local_cnt != NULL) && (len <= gasnetc_bounce_limit))) {
+	(is_nonbulk && (len <= gasnetc_bounce_limit))) {
     sreq->fh_count = 1; /* Just the remote one */
   } else {
     size_t new_len = gasnetc_get_local_fh(sreq, loc_addr, len);
@@ -2219,7 +2220,7 @@ size_t gasnetc_fh_put_helper(
       if (remote_cnt != NULL) {
 	++(*remote_cnt);
       }
-    } else if ((nbytes <= gasnetc_bounce_limit) && (local_cnt != NULL)) {
+    } else if (is_nonbulk && (nbytes <= gasnetc_bounce_limit)) {
       /* Bounce buffer use for non-bulk puts (upto a limit) */
       sreq->opcode = is_long_payload ? GASNETC_OP_LONG_BOUNCE : GASNETC_OP_PUT_BOUNCE;
       if_pf (fh_rem == NULL) { /* Memory will be copied asynchronously */
@@ -3158,7 +3159,8 @@ extern int gasnetc_rdma_put(
     // Also use bounce buffers if (firehose disabled AND src is in neither the client
     // nor aux segment) OR zero copy fails such as for read-only memory (bug 3338).
     size_t to_xfer = nbytes;
-    if ((nbytes <= gasnetc_bounce_limit) ||
+    int is_nonbulk = (local_cb == gasnetc_cb_counter); // GEX_EVENT_NOW
+    if ((is_nonbulk && (nbytes <= gasnetc_bounce_limit)) ||
         (!GASNETC_USE_FIREHOSE &&
          !gasnetc_in_bound_segment(ep, (uintptr_t)src_ptr, nbytes) &&
          !gasneti_in_local_auxsegment((gasneti_EP_t)ep, src_ptr, nbytes)) ||
@@ -3225,7 +3227,8 @@ extern int gasnetc_rdma_long_put(
   // Also use bounce buffers if (firehose disabled AND src is in neither the client
   // nor aux segment) OR zero copy fails such as for read-only memory (bug 3338).
   size_t to_xfer = nbytes;
-  if ((nbytes <= gasnetc_bounce_limit) ||
+  int is_nonbulk = (local_cb == gasnetc_cb_counter); // GEX_EVENT_NOW
+  if ((is_nonbulk && (nbytes <= gasnetc_bounce_limit)) ||
       (!GASNETC_USE_FIREHOSE &&
        !gasnetc_in_bound_segment(ep, (uintptr_t)src_ptr, nbytes) &&
        !gasneti_in_local_auxsegment((gasneti_EP_t)ep, src_ptr, nbytes)) ||
