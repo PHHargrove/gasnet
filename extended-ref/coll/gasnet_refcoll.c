dceb2c93bc5c464ed23692dcf633b890a9376346
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-07-20 22:35:01 -0700

    coll/reduce: new ReduceToAll dispatcher
    This commit resolves bug 4266:
      "gex_Coll_ReduceToAllNB is not thread-safe"
    by discarding the flawed implementation built on subordinate
    collectives with use of the newly generalizes Binomial tree
    implementations of ReduceToOne (now capable of implementing either
    ToOne or ToAll).

--- extended-ref/coll/gasnet_refcoll.c
+++ extended-ref/coll/gasnet_refcoll.c
@@ -2539,13 +2539,44 @@ gasnete_tm_reduce_all_nb_default(
     return GEX_EVENT_INVALID;
   }
 
-  // TODO-EX: replace this correct-but-horrible implementation:
-  gex_Event_t result =
-         gasnete_tm_reduce_all_Bcast(e_tm, dst, src, dt, dt_sz, dt_cnt,
-                                     opcode, user_fnptr, user_cdata,
-                                     0, NULL, 0 GASNETI_THREAD_PASS);
+  // Some ReduceToOne algorithms will implement ReduceToAll if passed
+  // (root == GEX_RANK_INVALID).  However, that has a different signature
+  // from implementations which implement only ReduceToAll.  So, the
+  // algorithm selection will set exactly one of the following.
+  gasnete_tm_reduce_fn_ptr_t to_one_alg = NULL;
+  gasnete_tm_reduce_all_fn_ptr_t to_all_alg = NULL;
 
-  GASNETE_COLL_CHECK_NO_SCRATCH((gasnet_team_handle_t)i_tm->_coll_team); // All reduce-to-all calls "count"
+  // TODO-EX: LUB can be relaxed (potentially significantly) for pshm-only teams
+  // TODO-EX: smarter implementations, such as dissemination for idempotent ops
+  gasnete_coll_team_t team = i_tm->_coll_team;
+  const size_t nbytes = dt_sz * dt_cnt;
+  const int binomial_root_radix = 1 + gasnete_coll_log2_rank(i_tm->_size - 1);
+  if ((nbytes * binomial_root_radix <= team->p2p_eager_buffersz) &&
+      (nbytes <= gex_AM_LUBRequestMedium())) {
+    to_one_alg = &gasnete_tm_reduce_BinomialEager;
+  } else if ((dt_sz * binomial_root_radix <= team->p2p_eager_buffersz) &&
+             (dt_sz <= gex_AM_LUBRequestMedium())) {
+    to_one_alg = &gasnete_tm_reduce_BinomialEagerSeg;
+  } else {
+    gasneti_assert(dt == GEX_DT_USER);
+    gasneti_fatalerror("gex_Coll_ReduceToAllNB: (dt_sz == %"PRIuSZ") is TOO LARGE for this implementation",
+                       dt_sz);
+  }
+
+  gasneti_assert(!!to_one_alg ^ !!to_all_alg); // exactly one must be non-NULL
+
+  gex_Event_t result;
+  if (to_one_alg) {
+    result = (*to_one_alg)(e_tm, GEX_RANK_INVALID, dst, src,
+                           dt, dt_sz, dt_cnt,
+                           opcode, user_fnptr, user_cdata,
+                           /*flags*/0, NULL, sequence GASNETI_THREAD_PASS);
+  } else {
+    result = (*to_all_alg)(e_tm, dst, src,
+                           dt, dt_sz, dt_cnt,
+                           opcode, user_fnptr, user_cdata,
+                           /*flags*/0, NULL, sequence GASNETI_THREAD_PASS);
+  }
 
   gasneti_AMPoll(); // No progress made until now
   return result;
