44f07ac9ef542f04c3af9377184befdff07bd3e9
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-08-07 00:59:23 -0700

    Document GEX_FLAG_NEVER_MY_{RANK,NBRHD} plans

--- extended-ref/gasnet_extended_help.h
+++ extended-ref/gasnet_extended_help.h
@@ -423,6 +423,11 @@ GASNETI_PUREP(gasneti_nbrhd_mapped_addr_or_null)
 // This excludes device memory in any process.
 // It returns the local address if addressible, and NULL otherwise.
 // This is based on the jobrank and memory kind.
+//
+// TODO: A `GEX_FLAG_NEVER_MY_{RANK,NBRHD}` pair of flags could/should be
+// implemented to provide a replacement for the functionality once provided by
+// `GASNETE_PUTGET_ALWAYSREMOTE`, but doing so on a per-call basis rather then
+// per translation unit.
 GASNETI_INLINE(gasnete_mapped_at) GASNETI_PURE
 void *gasnete_mapped_at(gex_TM_t _e_tm, gex_Rank_t _rank, const void *_addr) {
     gasneti_assume(_addr != NULL);
