7afa468b0e8bff1496b1f22a88affebfe70566a0
Paul H. Hargrove <PHHargrove@lbl.gov>
2022-05-19 22:49:21 -0700

    Add bounded retry to gasneti_gethostid()
    This commit adds logic to tolerate transient misbehavior from
    `gethostid()`, consisting of bounded retry with an exponentially
    growing delay between attempts.
    
    Additionally, this commit suppresses the console message about a
    bad return from `gethostid()` for the cases in which it was not
    actually called.

--- gasnet_internal.c
+++ gasnet_internal.c
@@ -1888,9 +1888,17 @@ extern uint32_t gasneti_gethostid(void) {
     static uint32_t myid = 0;
 
     if_pf (!myid) {
-    #if PLATFORM_OS_CYGWIN
-      /* gethostid() is known to be unreliable - we'll hash the hostname */
-    #elif HAVE_GETHOSTID
+    #if PLATFORM_OS_CYGWIN || !HAVE_GETHOSTID
+      // gethostid() is known to be unreliable or unavailable - always hash the hostname
+      #undef GASNETI_GETHOSTID_RETRIES
+    #else
+      // gethostid() is available - hash the hostname only if it looks unreliable after multiple retries
+      #ifndef GASNETI_GETHOSTID_RETRIES
+        #define GASNETI_GETHOSTID_RETRIES 24 // try to keep worst case delay under 0.1s
+      #endif
+      int retries = GASNETI_GETHOSTID_RETRIES;
+      uint64_t delay_ns = 1;
+    retry:
       myid = (uint32_t)gethostid();
     #endif
 
@@ -1908,11 +1916,21 @@ extern uint32_t gasneti_gethostid(void) {
           || (myid == 0x0000017f)
           || (myid == 0x0001007f)
           || (myid == 0x0100007f)) {
+      #if GASNETI_GETHOSTID_RETRIES
+        if (retries-- > 0) {
+          GASNETI_TRACE_PRINTF(I,("Retrying after invalid return 0x%08x from gethostid()", (unsigned int)myid));
+          gasneti_nsleep(delay_ns);
+          delay_ns *= 2;
+          goto retry;
+        }
+      #endif
+      #if HAVE_GETHOSTID && !PLATFORM_OS_CYGWIN
         gasneti_console_message("WARNING", "Invalid return 0x%08x from gethostid().  "
                                 "Please see documentation on GASNET_HOST_DETECT in README and "
                                 "consider setting its value to 'hostname' or reconfiguring using "
                                 "'--with-host-detect=hostname' to make that the default value.",
                                 (unsigned int)myid);
+      #endif
         uint64_t csum = gasneti_hosthash();
         myid = GASNETI_HIWORD(csum) ^ GASNETI_LOWORD(csum);
       }
