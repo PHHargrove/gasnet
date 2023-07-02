8c2f7a4052dbaff7961ba876ffd6056444be8190
Paul H. Hargrove <PHHargrove@lbl.gov>
2023-07-01 17:34:16 -0700

    testinternal: bound progress function comms
    This commit ensures the progress functions subtests of `testinternal`
    perform only only "bounded communications".  There are two ways in
    which communications is bounded.
    
    The addition of a counter check ensures that each progress function
    will only run a bounded number of times.  This bounds the total
    volume of communication generated by each progress function and helps
    to ensure eventual termination of the test even if a conduit cannot
    retire at least as much work per AMPoll as the progress function
    creates (locally or at its peer).
    
    While existing event handling logic served to bound to number of RMA
    operations from the progress function in-flight simultaneously, there
    was previously no such bound on AM operations.  This commit adds such
    a bound, though only indirectly by limiting the difference between the
    counts of requests sent and replies received.  Unlike the bound on the
    total volume of communication this is not required to ensure the test
    can terminate.  However, it can make the test far less punishing for
    some conduits (ucx in EVERYTHING segment mode, in particular).

--- gasnet_diagnostic.c
+++ gasnet_diagnostic.c
@@ -894,19 +894,15 @@ static void progressfn_reph(gex_Token_t token, void *buf, size_t nbytes) {
   gasneti_weakatomic_increment(&progressfn_rep_rcvd,0);
 }
 static void progressfn_tester(int *counter) {
+  const int max_runs = 100000; // bound total work injected and prevent counter overflow
   static int active = 0; /* protocol provides mutual exclusion & recursion protection */
   int iamactive = 0;
   gex_HSL_Lock(&pf_lock);
-    (*counter)++;
-    if (!active) { active = 1; iamactive = 1; }
+    if (!active && (*counter < max_runs)) { active = 1; iamactive = 1; (*counter)++; }
   gex_HSL_Unlock(&pf_lock);
   if (!iamactive) return;
 
   /* do some work that should be legal inside a progress fn */
-#if 0
-  if ((gasneti_weakatomic_read(&progressfn_req_sent,0) -
-       gasneti_weakatomic_read(&progressfn_rep_rcvd,0)) <= 128U)
-#endif
   { static int tmp;
     // The outer context in progressfns_test() is performing RMA operations
     // which reference only the bottom halves of our segment and of the
@@ -933,7 +929,9 @@ static void progressfn_tester(int *counter) {
       }
       pf_events[1] = gasnete_end_nbi_accessregion(0 GASNETI_THREAD_GET);
     }
-    if (gasneti_diag_havehandlers) {
+    if (gasneti_diag_havehandlers &&
+        ((gasneti_weakatomic_read(&progressfn_req_sent,0) -
+          gasneti_weakatomic_read(&progressfn_rep_rcvd,0)) <= 128U)) {
       const size_t max_sz = MIN(gex_AM_MaxRequestMedium(myteam, peer, GEX_EVENT_NOW, 0, 0),
                                 MIN(64*1024,region_len));
       for (size_t sz = 1; sz <= max_sz; sz = (sz < 64?sz*2:sz*8)) {
